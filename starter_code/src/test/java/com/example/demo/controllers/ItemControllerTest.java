0a161c58877ca8773b7920766b74a3c56ada3ee9
lama <Lamaalsultan999@gmail.com>
2024-06-15 21:41:54 +0300

    add code to github

---
+++ starter_code/src/test/java/com/example/demo/controllers/ItemControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.controllers;
+
+import junit.framework.TestCase;
+
+public class ItemControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetItems() {
+    }
+
+    public void testGetItemById() {
+    }
+
+    public void testGetItemsByName() {
+    }
+}
---
+++ starter_code/src/test/java/com/example/demo/controllers/OrderControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import junit.framework.TestCase;
+
+public class OrderControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testSubmit() {
+    }
+
+    public void testGetOrdersForUser() {
+    }
+}
